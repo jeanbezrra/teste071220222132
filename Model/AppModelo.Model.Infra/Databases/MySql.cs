b0be5d93012bf9de9bf068389881a6e86ada0034
ItaloCostaSantos <italosantoscosta07@gmail.com>
2022-10-18 21:56:47 -0300

    Criando conexão com banco de dados

---
+++ Model/AppModelo.Model.Infra/Databases/MySql.cs
@@ -0,0 +1,11 @@
+﻿namespace AppModelo.Model.Infra.Databases
+{
+    public static class MySql
+    {
+        public static string ConectionString()
+        {
+            var conn = "server=mysql.wwonline.com.br;database=wwonline05;uid=wwonline05;password=aluno22senai;";
+            return conn;
+        }
+    }
+}
--- Model/AppModelo.Model.Infra/Repositories/NacionalidadeRepository.cs
+++ Model/AppModelo.Model.Infra/Repositories/NacionalidadeRepository.cs
@@ -1,18 +1,45 @@
 ﻿using AppModelo.Model.Domain.Entities;
+using Dapper;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
+using System.Data;
 
 namespace AppModelo.Model.Infra.Repositories
 {
     public class NacionalidadeRepository
     {
         // insert (create) - select (read) - update (update) - delete (delete)
-        public bool Inserir() { }
-        public bool Atualizar() { }
-        public bool Remover() { }
-        public List<NacionalidadeEntity> ObterTodos() 
+        public bool Inserir(string descricao) 
+        {
+            //string interpolation $
+            var sql = $"INSERT INTO nacionalidades (descricao) VALUES ('{descricao}')";
+
+            using IDbConnection conexaoBd = new MySqlConnection(Databases.MySql.ConectionString());
+            var resultado = conexaoBd.Execute(sql);
+
+            return resultado > 0;
+        }
+        public bool Atualizar() 
+        {
+            return false;
+        }
+        public bool Remover() 
+        {
+            return false;
+        }
+        public IEnumerable<NacionalidadeEntity> ObterTodos()
         {
             var sql = "SELECT * FROM nacionalidades";
+
+            using IDbConnection conexaoBd = new MySqlConnection(Databases.MySql.ConectionString());
+            //todo comando select usa-se .Query<>
+            var resultado = conexaoBd.Query<NacionalidadeEntity>(sql);
+
+            return resultado;
         }
-        public NacionalidadeEntity ObterPorId() { }
+        public NacionalidadeEntity ObterPorId() 
+        {
+            return new NacionalidadeEntity();
+        }
     }
 }
